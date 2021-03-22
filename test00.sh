2d2a8862d87cd18f334bc6f56731ba3d91a9a4bb
z5288155 <z5288155@cse.unsw.edu.au>
2021-03-22 16:44:26 +1100

    Fixed bug of commit before add

--- test00.sh
+++ test00.sh
@@ -1,28 +1,37 @@
-res="commit_res.txt"
-rm $res
+clear
 rm -rf .girt
 rm a 
 
 echo "line 1" > a 
-./girt-commit -m 'test'
 
+echo "|---------- Commit before init -----------|"
+./girt-commit -m 'test'
 ./girt-init
 
-# commit before girt-add
-./girt-commit -m 'test commit' >> $res
+echo "|---------- Commit before add ------------|"
+./girt-commit -m 'test commit' 
 
+echo "|---------- Commit after add -------------|"
 ./girt-add a 
-./girt-commit -m 'test commit' >> $res
+./girt-commit -m 'test commit' 
 
-# correct cases
-./girt-commit -a -m 'test commit' > $res
-./girt-commit -m 'test commit' >> $res
+echo "|---------- Commit without changes -------|"
+./girt-commit -m 'test commit' 
 
-# special cases                             # give "usage: girt-commit [-a] -m commit-message"
-./girt-commit -m 'test commit' -a >> $res   # -a should be first
-./girt-commit -a -m >> $res                 # commit message should not be empty
-./girt-commit -a 'test commit' -m >> $res   # commit message should not be empty
-./girt-commit -a -m -d >> $res              # commit message should not be start with '-'
-./girt-commit -m -d >> $res                 # should not contain other options
-./girt-commit -a -d -m >> $res              # should not contain other options
+echo "|---------- Add new commit ---------------|"
+echo "line 2" >> a                          # update a
+echo "line 1" > b  
+./girt-commit -a -m 'test commit'    # update a in index with content of a in root 
+cat a
+cat .girt/index/a 
+
+
+echo "|---------- Special cases -----------------|"              
+# give "usage: girt-commit [-a] -m commit-message"
+./girt-commit -m 'test commit' -a    # -a should be first
+./girt-commit -a -m                  # commit message should not be empty
+./girt-commit -a 'test commit' -m    # commit message should not be empty
+./girt-commit -a -m -d               # commit message should not be start with '-'
+./girt-commit -m -d                  # should not contain other options
+./girt-commit -a -d -m               # should not contain other options
 
