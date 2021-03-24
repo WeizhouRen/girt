aedacfb919d1cd95fb84f3a64cf87d92e72f5c91
z5288155 <z5288155@cse.unsw.edu.au>
2021-03-24 19:45:48 +1100

    finish subset 1

--- girt-rm-test.sh
+++ girt-rm-test.sh
@@ -30,6 +30,51 @@ clear
 
 
 ################# TEST SUBSET1_18 #################
+# ./girt-init
+# # Initialized empty girt repository in .girt
+# echo 1 >a
+# echo 2 >b
+# echo 3 >c
+# ./girt-add a b c
+# ./girt-commit -m "first commit"
+# # Committed as commit 0
+# echo 4 >>a
+# echo 5 >>b
+# echo 6 >>c
+# echo 7 >d
+# echo 8 >e
+# ./girt-add b c d
+# echo 9 >b
+# ./girt-rm a
+# # girt-rm: error: 'a' in the repository is different to the working file
+# ./girt-rm b
+# # girt-rm: error: 'b' in index is different to both to the working file and the repository
+# ./girt-rm c
+# # girt-rm: error: 'c' has staged changes in the index
+# ./girt-rm d
+# # girt-rm: error: 'd' has staged changes in the index
+# ./girt-rm e
+# # girt-rm: error: 'e' is not in the girt repository
+# ./girt-rm --cached a
+# ./girt-rm --cached b
+# # # girt-rm: error: 'b' in index is different to both to the working file and the repository
+# ./girt-rm --cached c
+# ./girt-rm --cached d
+# ./girt-rm --cached e
+# # girt-rm: error: 'e' is not in the girt repository
+# ./girt-rm --force a
+# # girt-rm: error: 'a' is not in the girt repository
+# # ./girt-rm --force b
+# # ./girt-rm --force c
+# # # girt-rm: error: 'c' is not in the girt repository
+# # ./girt-rm --force d
+# # # girt-rm: error: 'd' is not in the girt repository
+# # ./girt-rm --force e
+# # # girt-rm: error: 'e' is not in the girt repository
+
+###################################################
+
+
 ./girt-init
 # Initialized empty girt repository in .girt
 echo 1 >a
@@ -43,33 +88,9 @@ echo 5 >>b
 echo 6 >>c
 echo 7 >d
 echo 8 >e
-./girt-add b c d
+./girt-add b c d e
 echo 9 >b
-./girt-rm a
-# girt-rm: error: 'a' in the repository is different to the working file
-./girt-rm b
-# girt-rm: error: 'b' in index is different to both to the working file and the repository
-./girt-rm c
-# girt-rm: error: 'c' has staged changes in the index
-./girt-rm d
-# girt-rm: error: 'd' has staged changes in the index
-./girt-rm e
-# girt-rm: error: 'e' is not in the girt repository
-./girt-rm --cached a
-./girt-rm --cached b
-# # girt-rm: error: 'b' in index is different to both to the working file and the repository
-./girt-rm --cached c
-./girt-rm --cached d
-./girt-rm --cached e
-# girt-rm: error: 'e' is not in the girt repository
-./girt-rm --force a
-# girt-rm: error: 'a' is not in the girt repository
-# ./girt-rm --force b
-# ./girt-rm --force c
-# # girt-rm: error: 'c' is not in the girt repository
-# ./girt-rm --force d
-# # girt-rm: error: 'd' is not in the girt repository
-# ./girt-rm --force e
-# # girt-rm: error: 'e' is not in the girt repository
-
-###################################################
+echo 0 >d
+./girt-rm --cached a c
+# ./girt-rm --force --cached b
+# ./girt-rm --force --cached e
--- girt-status-test.sh
+++ girt-status-test.sh
@@ -1,19 +1,21 @@
 rm -rf .girt
+rm a b c d e f g h
 clear
-./girt-init
-touch a b c d e f g h
-./girt-add a b c d e f
-./girt-commit -m 'first commit'
-# Committed as commit 0
-echo hello >a
-echo hello >b
-echo hello >c
-./girt-add a b
-echo world >a
-rm d
-./girt-rm e
-./girt-add g
-# ./girt-status
+# ./girtt-init
+# touch a b c d e f g h
+# ./girtt-add a b c d e f
+# ./girtt-commit -m 'first commit'
+# # Committed as commit 0
+# echo hello >a
+# echo hello >b
+# echo hello >c
+# ./girtt-add a b
+# echo world >a
+# rm d
+# ./girtt-rm e
+# ./girtt-add g
+# ./girtt-status
+
 # a - file changed, different changes staged for commit
 # b - file changed, changes staged for commit
 # c - file changed, changes not staged for commit
@@ -33,3 +35,26 @@ rm d
 # girt-status - untracked
 # girt.py - untracked
 # h - untracked
+
+
+
+######## Subset1_23 ###########
+./girt-init
+# Initialized empty .girt repository in .girt
+echo hi >a
+./girt-add a
+./girt-commit -m message
+# Committed as commit 0
+echo hello >b
+echo hola >c
+./girt-add b c
+./girt-status
+# a - same as repo
+# b - added to index
+# c - added to index
+# echo there >>b
+# rm c
+# ./girt-status
+# a - same as repo
+# b - added to index, file changed
+# c - added to index, file deleted
