b81b9490e1f4744c7df05b0c7ae54d523382b39c
z5288155 <z5288155@cse.unsw.edu.au>
2021-03-31 17:00:13 +1100

    pass 4 tests in subset2

--- 2041-girt-checkout-test.sh
+++ 2041-girt-checkout-test.sh
@@ -1,38 +1,43 @@
-
-
-
-# subset2_24
-rm -rf .girt
-clear
 2041 girt-init
 # Initialized empty girt repository in .girt
-touch a
+touch a b c
 2041 girt-add a
-2041 girt-commit -m commit-0
+2041 girt-commit -m commit-A
 # Committed as commit 0
 2041 girt-branch b1
 2041 girt-checkout b1
 # Switched to branch 'b1'
-touch b
-2041 girt-add b
-2041 girt-commit -m commit-1
-# Committed as commit 1
-2041 girt-checkout master
-# Switched to branch 'master'
-2041 girt-branch b2
-2041 girt-checkout b2
-# Switched to branch 'b2'
-touch c
-2041 girt-add c
-2041 girt-commit -m commit-2
-# Committed as commit 2
-2041 girt-branch
-# b1
-# b2
-# master
-2041 girt-checkout b1
-Switched to branch 'b1'
-2041 girt-checkout master
-# Switched to branch 'master'
-2041 girt-checkout non-existent-branch
-# 2041 girt-checkout: error: unknown branch 'non-existent-branch'
+touch d e
+# 2041 girt-rm a b
+# # girt-rm: error: 'b' is not in the girt repository
+# 2041 girt-commit -m commit-B
+# # nothing to commit
+# 2041 girt-checkout master
+# # Switched to branch 'master'
+# 2041 girt-branch b2
+# 2041 girt-checkout b2
+# # Switched to branch 'b2'
+# touch f g
+# 2041 girt-rm b c
+# # girt-rm: error: 'b' is not in the girt repository
+# 2041 girt-add f g
+# 2041 girt-commit -m commit-C
+# # Committed as commit 1
+# 2041 girt-branch
+# # b1
+# # b2
+# # master
+# 2041 girt-checkout b1
+# # Switched to branch 'b1'
+# 2041 girt-checkout master
+# # Switched to branch 'master'
+# 2041 girt-checkout b2
+# # Switched to branch 'b2'
+# 2041 girt-checkout b1
+# # Switched to branch 'b1'
+# 2041 girt-checkout master
+# # Switched to branch 'master'
+# 2041 girt-checkout b2
+# # Switched to branch 'b2'
+# 2041 girt-checkout b1
+# # Switched to branch 'b1'
